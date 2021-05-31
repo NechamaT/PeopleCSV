039f4dde39994bda9b48365c664481010e6d6a35
NechamaT <80014958+NechamaT@users.noreply.github.com>
2021-05-30 20:31:07 -0400

    ppl cards

--- PeopleCSV.Web/ClientApp/src/Pages/Home.js
+++ PeopleCSV.Web/ClientApp/src/Pages/Home.js
@@ -22,18 +22,20 @@ const Home = () =>{
 
 return(
     <div>
-    <div className="container" style={{marginTop: 20}}>
+    
         <div className="row col-md-13" style={{alignContent:"center" }}>
             <button className="btn btn-danger btn-block" onClick={onDelete}>Delete All</button>
         </div>
-     
+     <div className="row">
           {!!people.length &&
             people.map((person) => (
               <PersonRow person={person} key={person.id} />
             ))}
+            </div>
+            </div>
       
-    </div>
-  </div>
+  
+
 )
 }
 
--- PeopleCSV.Web/ClientApp/src/components/PersonRow.js
+++ PeopleCSV.Web/ClientApp/src/components/PersonRow.js
@@ -3,23 +3,20 @@ import React from "react";
 const PersonRow = ({ person }) => {
   const { firstName, lastName, email, age, address } = person;
   return (
-
-<div className="card mb-3" style={{maxWidth:540}}>
-<div className="row g-0">
-  <div className="col-md-4">
-    <img src='https://www.pngitem.com/pimgs/m/522-5220445_anonymous-profile-grey-person-sticker-glitch-empty-profile.png'/>
+<div className="container col-md-3 col-mt-4">
+<div className="card">
+  <img src="https://www.pngitem.com/pimgs/m/522-5220445_anonymous-profile-grey-person-sticker-glitch-empty-profile.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{firstName} {lastName}</h5>
   </div>
-  <div className="col-md-8">
-    <div className="card-body">
-      <h5 className="card-title">{firstName} {lastName}</h5>
-      <p className="card-text">Age: {age}</p>
-      <p className="card-text">Address: {address}</p>
-      <p className="card-text">Email: {email}</p>
-    </div>
-  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">Age: {age}</li>
+    <li className="list-group-item">Email: <br/> {email}</li>
+    <li className="list-group-item">Address: {address}</li>
+  </ul>
 </div>
 </div>
-  );
+);
   }
 
 export default PersonRow;
